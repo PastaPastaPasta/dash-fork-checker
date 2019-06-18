80c35719a9134e6b919cf36eae5f7e53617c232f
Pasta <pasta@dashboost.org>
2019-06-17 20:31:14 -0500

    add some returns and decomment one check

--- check.py
+++ check.py
@@ -18,21 +18,27 @@ def main(tried):
     if not dashevo_insight == blockchair:
         if tried:
             send_notification("Dashevo insights latest block hash (" + dashevo_insight + ") does not equal blockchairs latest hash (" + blockchair + ")")
+            return
         else: 
             time.sleep(wait_time)
             main(True)
+            return
     if not blockchair == chainz:
         if tried:
             send_notification("Blockchairs latest block hash (" + blockchair + ") does not equal Chainzs latest hash (" + chainz + ")")
+            return
         else: 
             time.sleep(wait_time)
             main(True)
-#    if not chainz == blockcypher:
-#        if tried:
-#            send_notification("Chainzs latest block hash (" + chainz + ") does not equal blockcyphers latest hash (" + blockcypher + ")")
-#        else: 
-#            time.sleep(wait_time)
-#            main(True)
+            return
+    if not chainz == blockcypher:
+        if tried:
+            send_notification("Chainzs latest block hash (" + chainz + ") does not equal blockcyphers latest hash (" + blockcypher + ")")
+            return
+        else: 
+            time.sleep(wait_time)
+            main(True)
+            return
 
 # Sends a slack notification with the webhook being in secret.txt
 def send_notification(text):
@@ -40,7 +46,6 @@ def send_notification(text):
     if secret_file.mode == 'r':
         secret = secret_file.read()
 
-    print("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" " + text + " \"}' " + secret)
     os.popen("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\" " + text + " \"}' " + secret)
 
 if __name__ == "__main__":
