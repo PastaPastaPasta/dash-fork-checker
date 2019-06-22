43db950107598bcbed9e9d2930e60f7a8e9169e0
Pasta <pasta@dashboost.org>
2019-06-22 15:48:54 -0500

    implement requests based setup, plus height + hash detection

--- check.py
+++ check.py
@@ -1,48 +1,83 @@
 import os
 import time
-
+import requests
+import json
+import pprint
+from lxml import html
 
 def main(tried):
-    cmd_dashevo_insight = "curl -s https://insight.dashevo.org/insight-api/status?q=getBestBlockHash | jq .bestblockhash ;"
-    cmd_blockchair = "curl -s https://api.blockchair.com/dash/stats | jq '.data.best_block_hash' ;"
-    cmd_chainz = "curl -s https://chainz.cryptoid.info/dash/api.dws?q=getblockcount | xargs -I{} echo {}+1 | bc | xargs -I{} curl -s 'https://chainz.cryptoid.info/dash/api.dws?q=getblockhash&height={}' | jq ."
-    cmd_blockcypher = "curl -s https://api.blockcypher.com/v1/dash/main | jq .hash"
-    cmd_trezor1 = "curl -s https://dash1.trezor.io/blocks | grep -oP '(?<=<td class=\"ellipsis\">).*?(?=</td>)' | head -1 | xargs -I{} echo \\\"{}\\\" | jq . ;"
-    cmd_trezor2 = "curl -s https://dash2.trezor.io/blocks | grep -oP '(?<=<td class=\"ellipsis\">).*?(?=</td>)' | head -1 | xargs -I{} echo \\\"{}\\\" | jq . ;"
-    cmd_trezor3 = "curl -s https://dash3.trezor.io/blocks | grep -oP '(?<=<td class=\"ellipsis\">).*?(?=</td>)' | head -1 | xargs -I{} echo \\\"{}\\\" | jq . ;"
-    cmd_trezor4 = "curl -s https://dash4.trezor.io/blocks | grep -oP '(?<=<td class=\"ellipsis\">).*?(?=</td>)' | head -1 | xargs -I{} echo \\\"{}\\\" | jq . ;"
-    cmd_trezor5 = "curl -s https://dash5.trezor.io/blocks | grep -oP '(?<=<td class=\"ellipsis\">).*?(?=</td>)' | head -1 | xargs -I{} echo \\\"{}\\\" | jq . ;"
 
-    data = {}
-    data["dashevo_insight"] = get_block_hash(cmd_dashevo_insight)
-    data["blockchair"] = get_block_hash(cmd_blockchair)
-    data["chainz"] = get_block_hash(cmd_chainz)
-#    data["blockcypher"] = get_block_hash(cmd_blockcypher)
-    blockcypher = get_block_hash(cmd_blockcypher)
-    data["trezor1"] = get_block_hash(cmd_trezor1)
-    data["trezor2"] = get_block_hash(cmd_trezor2)
-    data["trezor3"] = get_block_hash(cmd_trezor3)
-    data["trezor4"] = get_block_hash(cmd_trezor4)
-    data["trezor5"] = get_block_hash(cmd_trezor5)
+    url_dashevo_insight_hash = "https://insight.dashevo.org/insight-api/status?q=getBestBlockHash"
+    url_dashevo_insight_height = "https://insight.dashevo.org/insight-api/status?"
+    url_blockchair = "https://api.blockchair.com/dash/stats"
+    url_blockcypher = "https://api.blockcypher.com/v1/dash/main"
+    url_chainz_height = "https://chainz.cryptoid.info/dash/api.dws?q=getblockcount"
+    url_chainz_hash = "https://chainz.cryptoid.info/dash/api.dws?q=getblockhash&height="
+    url_trezor_prefix = "https://dash"
+    url_trezor_suffix = ".trezor.io/blocks"
+
+    block_height = {}
+    block_hash = {}
+
+    block_height["dashevo_insight"] = requests.get(url_dashevo_insight_height).json()['info']['blocks']
+    block_hash["dashevo_insight"] = requests.get(url_dashevo_insight_hash).json()['bestblockhash']
+
+    blockchair_data = requests.get(url_blockchair).json()
+    block_height["blockchair"] = blockchair_data['data']['best_block_height']
+    block_hash["blockchair"] = blockchair_data['data']['best_block_hash']
+
+    blockcypher_data = requests.get(url_blockcypher).json()
+    #block_height["blockcypher"] = blockcypher_data['height']
+    #block_hash["blockcypher"] = blockcypher_data['hash']
+    blockcypher = blockcypher_data['hash']
+
+    block_height["chainz"] = requests.get(url_chainz_height).json()
+    block_hash["chainz"] = requests.get('{}{}'.format(url_chainz_hash, block_height["chainz"])).json()
+
+    # Get data for trezor servers 1-5
+    for i in range(1, 6):
+        url_trezor = '{}{}{}'.format(url_trezor_prefix, i, url_trezor_suffix)
+        page = requests.get(url_trezor)
+        tree = html.fromstring(page.content)
+        tr_elements = tree.xpath('//tr')
+
+        # tr_elements[0][x] are the column names
+        trezor_name = '{}{}'.format("trezor", i)
+        block_height[trezor_name] = tr_elements[1][0].text_content()
+        block_hash[trezor_name] = tr_elements[1][1].text_content()
 
     # Sleep this many seconds to attempt to ensure one explorer isn't just behind
     wait_time = 10
 
     text = ""
 
-    uniqueValues = set(data.values())
-    
-    if len(uniqueValues) > 1:
-        text = "Possible fork! Explorers reporting different best block hashes.\n"
-        new_dict = {}
-        for k, v in data.items():
-            new_dict.setdefault(v, []).append(k)
+    uniqueValuesHeight = set(block_height.values())
+    uniqueValuesHash   = set(block_hash.values())
 
-        for item in new_dict.items():
+    if len(uniqueValuesHash) > 1:
+        if len(uniqueValuesHeight) > 1:
+            text = "[Unlikely] Possible fork! Explorers reporting different best block hashes and block heights.\n"
+        if len(uniqueValuesHeight) == 1:
+            text = "POSSIBLE FORK ALERT: Explorers reporting different best block hashes at the same height!!!"
+
+    if len(uniqueValuesHash) > 1:
+        new_dict_hash = {}
+        for k, v in block_hash.items():
+            new_dict_hash.setdefault(v, []).append(k)
+
+        for item in new_dict_hash.items():
             text = text + 'Hash: {} found on Explorer(s): {}\n'.format(item[0], ', '.join(item[1]))
 
-    if blockcypher == data["dashevo_insight"]:
-        text = text + 'Blockcypher (' + blockcypher + ')  appears up to date with dashevo (' + data["dashevo_insight"] + ')'
+    if len(uniqueValuesHeight) > 1:
+        new_dict_height = {}
+        for k, v in block_height.items():
+            new_dict_height.setdefault(v, []).append(k)
+
+        for item in new_dict_height.items():
+            text = text + 'Hash: {} found on Explorer(s): {}\n'.format(item[0], ', '.join(item[1]))
+
+    if blockcypher == block_hash["dashevo_insight"]:
+        text = text + 'Blockcypher (' + blockcypher + ')  appears up to date with dashevo (' + block_hash["dashevo_insight"] + ')'
 
     if not text == "":
         if tried:
@@ -50,9 +85,6 @@ def main(tried):
         else: 
             time.sleep(wait_time)
             main(True)
-
-def get_block_hash(cmd):
-    return os.popen(cmd).read().rstrip().strip('\"')
 
 # Sends a slack notification with the webhook being in secret.txt
 def send_notification(text):
